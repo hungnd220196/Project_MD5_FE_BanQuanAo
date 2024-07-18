47612f0f8b696e84bc49acbe63092cf42f943407
PhanTu2551993 <phanvantu25051993@gmail.com>
2024-07-18 17:14:28 +0700

    thu5

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,14 +2,15 @@ import { Button, Input, notification } from "antd";
 import "./index.scss";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { login } from "../../../services/authService";
-import { useNavigate } from "react-router-dom";
+import { login } from "../../services/authService";
+import { NavLink, useNavigate } from "react-router-dom";
+import { FacebookFilled, GoogleCircleFilled, LockOutlined, TwitterCircleFilled, UserOutlined } from "@ant-design/icons";
 
 export default function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [user, setUser] = useState({
-    email: "",
+    username: "",
     password: "",
   });
 
@@ -55,40 +56,60 @@ export default function Login() {
   };
 
   return (
-    <>
-      <div className="ra-login-container">
+    <div className="container">
+      <div className="form-wrapper">
+        <h2 className="title">Login</h2>
         <form className="form" onSubmit={handleSubmit}>
-          <h3 className="heading">Đăng nhập tài khoản</h3>
-
           <div className="form-group">
-            <label htmlFor="" className="form-label">
-              Email
+            <label htmlFor="username" className="form-label">
+              Username
             </label>
             <Input
               onChange={handleChange}
-              name="email"
+              name="username"
               className="form-input"
+              placeholder="Type your username"
+              prefix={<UserOutlined />}
+              value={user.username}
             />
-            {/* <p className="error-message">Email không để trống</p> */}
           </div>
           <div className="form-group">
-            <label htmlFor="" className="form-label">
-              Mật khẩu
+            <label htmlFor="password" className="form-label">
+              Password
             </label>
             <Input
               onChange={handleChange}
               name="password"
               className="form-input"
+              type="password"
+              placeholder="Type your password"
+              prefix={<LockOutlined />}
+              value={user.password}
             />
-            {/* <p className="error-message">Mật khẩu không để trống</p> */}
           </div>
-          <div className="button-login">
-            <Button htmlType="submit" type="primary">
-              Đăng nhập
-            </Button>
+          <div className="forgot-password">
+            <a href="#">Forgot password?</a>
           </div>
+          <Button className="button" htmlType="submit" type="primary">
+            LOGIN
+          </Button>
         </form>
+        <div className="social-login-text">Or Sign Up Using</div>
+        <div className="social-login">
+          <button className="social-button">
+          <FacebookFilled style={{color:"blue"}}/>
+          </button>
+          <button className="social-button">
+          <TwitterCircleFilled style={{color:"#2DA7EE"}}/>
+          </button>
+          <button className="social-button">
+          <GoogleCircleFilled style={{color:"#D64F44"}} />
+          </button>
+        </div>
+        <div className="sign-up">
+          Or Sign Up Using <NavLink to="/register">Register</NavLink><br /><br />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
